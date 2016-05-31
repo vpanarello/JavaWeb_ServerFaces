f34a72e0ea32ad7d4d5fc60665b3b4a14fff3186
Vagner Panarello Filho <RM30222@fiap.com.br>
2016-05-30 22:45:12 -0300

    atualizacao UsuariosBean

--- src/br/com/fiap/bean/UsuariosBean.java
+++ src/br/com/fiap/bean/UsuariosBean.java
@@ -3,6 +3,8 @@ package br.com.fiap.bean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
+import br.com.fiap.dao.GenericDao;
+import br.com.fiap.entity.Livro;
 import br.com.fiap.entity.Usuario;
 
 @ManagedBean
@@ -18,4 +20,24 @@ public class UsuariosBean {
 		this.usuario = usuario;
 	}
 	
+	public String cadastrarUsuario(){
+		GenericDao<Usuario> dao = new GenericDao<Usuario>(Usuario.class);
+		
+		try {
+			dao.adicionar(this.usuario);
+			return "sucesso";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "erro";
+		}
+	}
+	
+	public String validaUsuario(){
+		GenericDao<Usuario> dao = new GenericDao<Usuario>(Usuario.class);
+		
+		
+		
+		
+		return null;
+	}
 }
